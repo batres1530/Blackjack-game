937ea87e6721ffecb02e6b60b5283a3d5bf0e79d
David Martinez <sergiodavidbatresmartinez@gmail.com>
2025-01-14 07:33:05 -0600

    se creo la funcion de el valor de la carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -5,71 +5,65 @@
  2S =  es de espadas
  */
 
- let deck        = [];
-const tipos      = ['C', 'D', 'H', 'S'];
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
- // esta funcion crea el deck de cartas
- const crearDeck = () => {
-    
-     for (let i = 2; i <= 10; i++) {
-         for (let tipo of tipos) {
-             deck.push(i + tipo);
-         }
+// esta funcion crea el deck de cartas
+const crearDeck = () => {
+
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tipos) {
+            deck.push(i + tipo);
+        }
     }
-     for (let tipo of tipos) {
-         for (let esp of especiales) {
-             deck.push(esp + tipo);
-     }
+    for (let tipo of tipos) {
+        for (let esp of especiales) {
+            deck.push(esp + tipo);
+        }
     }
 
     deck = _.shuffle(deck);
     console.log(deck);
     return deck;
- };
-  
- crearDeck();
+};
 
-  //esta funcion me permite tomar una carta del deck
-  const  pedirCarta = () => {
-      // console.log(deck); aqui se muestra la baraja de cartas
-      // const carta = desck.unshift();
-      
-        if (deck.length === 0) {
-            throw 'No hay cartas en el deck';
-        }
+crearDeck();
 
-      const carta = deck.pop();
+//esta funcion me permite tomar una carta del deck
+const pedirCarta = () => {
+    // console.log(deck); aqui se muestra la baraja de cartas
+    // const carta = desck.unshift();
+
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+
+    const carta = deck.pop();
 
     console.log(carta); // carta debe ser de la baraja
     console.log(deck);
     return carta;
-  };
-
-//  pedirCarta();
- 
-const valorCarta = (carta) => {
-   const valor = carta.substring(0, carta.length - 1);
-   let puntos = 0;
-   
- if (isNaN(valor)) {
-     console.log('La carta no es un numero');
- } 
- else {
-    console.log('La carta es un numero');
-    puntos = valor *1;
-} 
- 
-    console.log(puntos);
-//    return valor;
 };
 
-valorCarta('kD');
+//  pedirCarta();
 
-throw 'el cookie muco se tardo en la cara';
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+
+    // if (isNaN(valor)) {
+    //     puntos = (valor === 'A') ? 11 : 10 +valor*1; 
+    // }
+    return (isNaN(valor)) ? (valor === 'A') ? 11 : 10 : valor * 1;
+};
+const valor = valorCarta(pedirCarta());
+console.log({ valor });
 
 
- 
 
 
-    
+
+
+
+
+
